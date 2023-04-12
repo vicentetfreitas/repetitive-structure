e4f52d041b448821f2e8b8e9cc87baeff3fa3e1c
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2023-04-11 21:38:16 -0300

    Repetitive structure while

---
+++ src/RepetitiveStructureWhile.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class RepetitiveStructureWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int sum = 0;
+        while (x != 0 ){
+            sum += x;
+            x = sc.nextInt();
+        }
+        System.out.println(sum);
+        sc.close();
+    }
+}
